cdd77a6428217398dcb5fff363fb7ac1d5da9cb9
seungminlee <seungminlee@nts-corp.com>
2018-10-04 22:59:56 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import {Router, browserHistory} from 'react-router'; /*2. 라이터 부분 추가*/
 
+/* 1. 리엑트 라우터로 적용해서 필요없는 부분*/
+/*import App from './components/app';*/
+import reducers from './reducers';
+ 
+//http://www.blog.com/#post/5  해시(#) 이후 모든것 감지 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+//<App /> 삭제
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} />
   </Provider>
   , document.querySelector('.container'));
+
+
+  /*
+75강
+  */
